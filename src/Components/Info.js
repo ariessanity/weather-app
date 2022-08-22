32d2201ac984f3a6c67ccfc371d859806754490c
Aries <ianorillo21@gmail.com>
2022-08-22 22:18:45 +0800

    adding responsive design

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -2,29 +2,23 @@ import React from "react";
 import "../Styles/Info.scss";
 
 function Info(props) {
-
-
   return (
-
     <div className="info">
 
-      
-
       <div className="info__up">
+        <div className="info__up--degrees">{props.data?.current?.temp_c}&deg;</div>
 
-        <div className="info__degrees">{props.data?.current?.temp_c}&deg;</div>
-
-        <div className="info__details">
-          <div className="info__city">
+        <div className="info__up--details">
+          <div className="info__up--details-city">
             {props.data?.location?.name}, {props.data?.location?.country}
             <p>{props.data?.location?.region}</p>
           </div>
-          <div className="info__date">{new Date().toDateString()}</div>
+          <div className="info__up--details-date">{new Date().toDateString()}</div>
         </div>
 
-        <div className="info__weather">
+        <div className="info__up--weather">
           <img
-            className="info__logo"
+            className="info__up--weather-logo"
             src={props.data?.current?.condition?.icon}
             alt=""
           />
@@ -33,25 +27,25 @@ function Info(props) {
       </div>
 
       <div className="info__down">
-        <div className="info__wind">
-          Wind <p>{props.data?.current?.wind_kph}</p>
+        <div className="info__down--status">
+          <div className="info__down--status-wind">
+            Wind <p>{props.data?.current?.wind_kph}</p>
+          </div>
+
+          <div className="info__down--status-humidity">
+            Humidity <p>{props.data?.current?.humidity}</p>
+          </div>
         </div>
 
-        <div className="info__humidity">
-          Humidity <p>{props.data?.current?.humidity}</p>
-        </div>
-
-        <div className="info__time">
-
-          
-          {new Date(props.data?.location?.localtime).toLocaleTimeString(navigator.language,{
-           
-            hour12: true,
-            hour: "numeric",
-            minute: "numeric",
-            
-          
-          })}
+        <div className="info__down--time">
+          {new Date(props.data?.location?.localtime).toLocaleTimeString(
+            navigator.language,
+            {
+              hour12: true,
+              hour: "numeric",
+              minute: "numeric",
+            }
+          )}
         </div>
       </div>
     </div>
